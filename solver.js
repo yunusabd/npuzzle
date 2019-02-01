bdb7e6f06097e2a26f40cad78aa66a68020e48b2
yunusabd <yunusemin-github@yahoo.com>
2019-02-01 22:59:37 +0100

    first commit

---
+++ solver.js
@@ -0,0 +1,16 @@
+console.log(data);
+
+function setup() {
+    createCanvas(640, 480);
+}
+
+var cols = 5;
+
+function draw() {
+    if (mouseIsPressed) {
+        fill(0);
+    } else {
+        fill(255);
+    }
+    ellipse(mouseX, mouseY, 80, 80);
+}
---
+++ solver.py
@@ -0,0 +1,35 @@
+import sys
+import numpy as np
+import json, codecs
+
+def parser():
+    # read from stdin
+    read = sys.stdin.read()
+    size = int(read.split('\n', 1)[0])
+    matrix_str = read[read.index("-----------") + len("-----------"):]
+    lines = matrix_str.split('\n')
+    cleaned = [line.strip() for line in lines if line != '']
+
+    # read cleaned data into list
+    data = []
+    for line in cleaned:
+        numbers = list(map(int, line.split()))
+        data.append(numbers)
+
+    arr = np.array(data)
+    shape = arr.shape
+
+    # test if arr is a square matrix
+    if shape[0] != shape[1] or shape[0] != size:
+        exit()
+
+    b = arr.tolist()
+    j = json.dumps(b, separators=(",", ":"), indent=4)
+    return j
+
+def writeFile():
+    f = open("data.js", "w+")
+    f.write("var data = " + str(parser()) + ";")
+
+if __name__ == '__main__':
+    writeFile()
