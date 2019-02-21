18499c4987df06412d3844c61514ad4cd597854f
Pablo Abril <pabril@student.42.fr>
2019-02-21 17:47:55 +0100

    update main puzzle dataclass

--- pythonImplementation/data_model.py
+++ pythonImplementation/data_model.py
@@ -1,10 +1,28 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Any
+from util import generate_permutations
 
 @dataclass(order=True)
 class PuzzleState:
     overall_cost: int
     moveNumber: int
-    size: int
     heuristicCost: int
+    size: int
     currentState: List[List[int]]
+    previous_state: Any
+
+    def __hash__(self):
+        return hash(str(self.currentState))
+
+    def is_final_state(self, solution):
+        return self.currentState == solution
+
+    def expand(self):
+        "generates possible transitions from a given state"
+        
+        pass
+
+@dataclass
+class ParsingError(Exception):
+    message: str
+
