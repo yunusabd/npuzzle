3ee61c8628061311fdf70b2aa2cff286c5ac6052
Pablo Abril <pabloabril75@gmail.com>
2019-02-23 11:14:05 +0100

    transform solver into a class

--- pythonImplementation/solver.py
+++ pythonImplementation/solver.py
@@ -1,55 +1,50 @@
 import heapq
 import random
 from data_model import PuzzleState
+from heuristic import *
 import util
 
-def manhattan(puzzle, solution):
-    cost = 0
-    for y in range(puzzle.size):
-        for x in range(puzzle.size):
-            digit = puzzle.currentState[y][x]
-            (expected_x, expected_y) = solution.positions[digit]
+class Astar:
+    def __init__(self, initial_puzzle):
+        self.initial_puzzle = initial_puzzle
 
-            x_distance = abs(x - expected_x)
-            y_distance = abs(y - expected_y)
+    def queue_push(self, value):
+        heapq.heappush(self.open_list, value)
 
-            cost += (x_distance + y_distance)
+    def queue_pop(self):
+        return heapq.heappop(self.open_list)
 
-    return cost
-
-
-def solve(initial_puzzle):
-    
-    open_list = []
-    closed_list = {}
-    success = 0
-    solution = util.generate_puzzle_solution(initial_puzzle.size)
-    print(solution.positions)
-
-    heapq.heappush(open_list, initial_puzzle)
-    while (len(open_list) != 0 and not success):
-        # Pop most valuable state.
-        current_state = heapq.heappop(open_list)
+    def solve(self):
         
+        self.open_list = []
+        self.closed_list = {}
+        self.solution = util.generate_puzzle_solution(self.initial_puzzle.size)
+        success = 0
 
-        # check if final state
-        if (current_state.is_final_state(solution.state)):
-            success = True
-        else:
-            # add to closed list.
-            closed_list[current_state] = current_state.overall_cost
+        self.queue_push(self.initial_puzzle)
+        while (len(self.open_list) != 0 and not success):
+            # Pop most valuable state.
+            current_state = self.queue_pop()
+            
 
-            # expand to next combinations.
-            for new_state in current_state.expand():
+            # check if final state
+            if (current_state.is_final_state(self.solution.state)):
+                success = True
+            else:
+                # add to closed list.
+                self.closed_list[current_state] = current_state.overall_cost
 
-                # compute new node heuristic cost.
-                new_state.compute_cost(solution, manhattan)
+                # expand to next combinations.
+                for new_state in current_state.expand():
 
-                # ignore state already in open/closed list.
-                if (new_state in closed_list):
-                    continue
+                    # compute new node heuristic cost.
+                    new_state.compute_cost(self.solution, manhattan_heuristic)
 
-                heapq.heappush(open_list, new_state)
+                    # ignore state already in open/closed list.
+                    if (new_state in self.closed_list):
+                        continue
 
-    if (success):
-        current_state.display_path()
+                    self.queue_push(new_state)
+
+        if (success):
+            current_state.display_path()
