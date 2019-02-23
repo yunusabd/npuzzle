2fbf76f249b8d890dc1374c9c43ae24c49b37f11
Pablo Abril <pabloabril75@gmail.com>
2019-02-23 10:02:59 +0100

    implement new Puzzle methods

--- pythonImplementation/data_model.py
+++ pythonImplementation/data_model.py
@@ -17,10 +17,31 @@ class PuzzleState:
     def is_final_state(self, solution):
         return self.currentState == solution
 
-    def expand(self):
+    def expand(self, heuristic):
         "generates possible transitions from a given state"
-        
-        pass
+        for child in generate_permutations(self):
+            # child.display()
+            child.previous_state = self
+            child.moveNumber = self.moveNumber + 1
+            child.size = self.size
+            yield child
+
+    def compute_cost(self, solution, heuristic):
+        # compute cost of node.
+        self.heuristicCost = heuristic(self.currentState, solution)
+        self.overall_cost = self.heuristicCost + self.moveNumber
+
+
+    def display(self):
+        "display current state of puzzle on screen"
+        for elem in self.currentState:
+            print(elem)
+        print("")
+
+    def display_path(self):
+        if (self.previous_state):
+            self.previous_state.display_path()
+        self.display()
 
 @dataclass
 class ParsingError(Exception):
