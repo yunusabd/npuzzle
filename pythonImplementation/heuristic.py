fa2b2e2356674cdce291134387faeb79985106c4
Pablo Abril <pabril@student.42.fr>
2019-02-27 14:40:23 +0100

    add hamming heuristic & fix manhattan

--- pythonImplementation/heuristic.py
+++ pythonImplementation/heuristic.py
@@ -3,10 +3,22 @@ def manhattan_heuristic(puzzle, solution):
     for y in range(puzzle.size):
         for x in range(puzzle.size):
             digit = puzzle.currentState[y][x]
+            if (digit == 0):
+                continue
             (expected_x, expected_y) = solution.positions[digit]
 
             x_distance = abs(x - expected_x)
             y_distance = abs(y - expected_y)
 
             cost += (x_distance + y_distance)
+    return cost
+
+def hamming_heuristic(puzzle, solution):
+    "return a cost equal to misplaced tiles."
+    cost = 0
+    for y in range(puzzle.size):
+        for x in range(puzzle.size):
+            digit = puzzle.currentState[y][x]
+            if (digit != solution.state[y][x]):
+                cost += 1
     return cost
