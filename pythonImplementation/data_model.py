337a41e06318718b3f81b76414dadc25683465aa
Pablo Abril <pabril@student.42.fr>
2019-03-21 17:00:14 +0100

    organize data model

--- pythonImplementation/data_model.py
+++ pythonImplementation/data_model.py
@@ -2,6 +2,18 @@ from dataclasses import dataclass
 from typing import List, Any, Dict
 from util import generate_permutations, flatten_nested_list
 
+
+@dataclass
+class PuzzleSolution:
+    "contains the finished state and a map to get locations of each digits."
+    state: List[List[int]]
+    positions: Dict
+
+@dataclass
+class ParsingError(Exception):
+    message: str
+
+# Dataclass decorator implements for me magic methods (__init__ , __eq__ , ....)
 @dataclass(order=True)
 class PuzzleState:
     overall_cost: int
@@ -21,6 +33,7 @@ class PuzzleState:
 
     def expand(self):
         "generates possible transitions from a given state"
+
         for child in generate_permutations(self):
             # child.display()
             child.previous_state = self
@@ -38,6 +51,21 @@ class PuzzleState:
         self.display_path()
         self.display_analytics(stats)
 
+    def display_move_number(self):
+        print("\n\t\t SOLUTION :\n")
+        print(f"Number of moves to solve the puzzle : {self.moveNumber}\n")
+
+    def display_path(self):
+        "recursively print path to solve puzzle"
+        if (self.previous_state):
+            self.previous_state.display_path()
+        self.display()
+    
+    def display_analytics(self, stats):
+        print("\t\t STATS :\n")
+        print(f"Total node selected in open list : {stats.expanded_node_number}")
+        print(f"Maximum amount of nodes in open list at same time: {stats.total_node_number}\n")
+
     def display(self):
         "display current state of puzzle on screen"
         width = self.size//2
@@ -48,28 +76,4 @@ class PuzzleState:
             print(f"{elem[-1]:{width}}]")
         print("")
 
-    def display_path(self):
-        if (self.previous_state):
-            self.previous_state.display_path()
-        self.display()
     
-    def display_move_number(self):
-        print("\n\t\t SOLUTION :\n")
-        print(f"Number of moves to solve the puzzle : {self.moveNumber}\n")
-    
-    def display_analytics(self, stats):
-        print("\t\t STATS :\n")
-        print(f"Total node selected in open list : {stats.expanded_node_number}")
-        print(f"Maximum amount of nodes in open list at same time: {stats.total_node_number}\n")
-
-
-@dataclass
-class PuzzleSolution:
-    "contains the finished state and a map to get locations of each digits."
-    state: List[List[int]]
-    positions: Dict
-
-@dataclass
-class ParsingError(Exception):
-    message: str
-
