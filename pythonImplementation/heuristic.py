ff9403f533f0448143e5e0d91e357e5f8c6ce230
Pablo Abril <pabril@student.42.fr>
2019-03-22 10:17:21 +0100

    finish linear conflict heuristic

--- pythonImplementation/heuristic.py
+++ pythonImplementation/heuristic.py
@@ -1,3 +1,5 @@
+import logging
+
 def manhattan_heuristic(puzzle, solution):
     cost = 0
     for row in range(puzzle.size):
@@ -40,70 +42,59 @@ def linear_conflict(puzzle, solution):
             cost += abs(row - expected_row)
 
             # Linear Conflict Part.
-
-            # In case already in solution position or not in solution row/column
-            if (row == expected_row):
-                # iterate on row
-                for index in range(puzzle.size):
-                    if (index == column or puzzle.currentState[row][index] == 0):
-                        continue
-                    compare_digit = puzzle.currentState[row][index]
-                    (compare_digit_expected_row, compare_digit_expected_column) = solution.positions[compare_digit]
-                    if (compare_digit_expected_row != expected_row):
-                        continue
-                    if ((compare_digit_expected_column >= column and column > index) or 
-                        (compare_digit_expected_column <= column and column < index)):
-                        puzzle.display()
-                        print(f"Linear conflict between {digit} & {compare_digit}")
-                        cost += 1
-
-    
-            # iterate on column
+            # If current tile on its expected column.
             if (column == expected_column):
-                for index in range(puzzle.size):
-                    if (index == row):
-                        continue
-                    pass
-
-
-            # for (uint8_t j=row * b._width, l=j + col; j < l; j++) {
-            #     if (b._tiles[j] == 0) {
-            #         continue
-            #     }
-            #     if (goalRow[j] == row & & goalCol[i] < goalCol[j]) {
-            #         md += 2
-            #     }
-            # }
+                cost += compute_vertical_conflict(puzzle, solution, digit, row, column, expected_column)
+            
+            # If current tile on its expected row.
+            if (row == expected_row):
+                cost += compute_horizontal_conflict(puzzle, solution, digit, row, column, expected_row)
 
     return cost
 
+def compute_horizontal_conflict(puzzle, solution, digit, row, column, expected_row):
+    cost = 0
+    for index in range(puzzle.size):
 
+        # skip if empty or reference tile.
+        if (index == column or puzzle.currentState[row][index] == 0):
+            continue
 
+        # get infos about tile.
+        compare_digit = puzzle.currentState[row][index]
+        (compare_digit_expected_row,
+            compare_digit_expected_column) = solution.positions[compare_digit]
 
+        # tmp tile goal position should be on same row.
+        if (compare_digit_expected_row != expected_row):
+            continue
 
-# def linear_conflict(puzzle, solution):
-#     linear_cost = 0
-#     manhattan_cost = manhattan_heuristic(puzzle, solution)
+        # if conflict
+        if ((index < column and column <= compare_digit_expected_column) or
+                (index > column and column >= compare_digit_expected_column)):
+            logging.debug(f"Horizontal conflict between {digit} & {compare_digit}")
+            cost += 2
+    return cost
 
-#     for y in range(puzzle.size):
-#         for x in range(puzzle.size):
-#             # check if belong to
-#             return
+def compute_vertical_conflict(puzzle, solution, digit, row, column, expected_column):
+    cost = 0
+    for index in range(puzzle.size):
 
-#     return manhattan_cost + linear_cost
+        # skip if empty or reference tile.
+        if (index == row or puzzle.currentState[index][column] == 0):
+            continue
 
-# def generate_pairs(puzzle, solution):
-#     "return list of tiles that are already in their final row/column"
-#     tiles_placed = []
+        # get infos about tile.
+        compare_digit = puzzle.currentState[index][column]
+        (compare_digit_expected_row, compare_digit_expected_column) = solution.positions[compare_digit]
 
-#     for row in range(puzzle.size):
-#         for column in range(puzzle.size):
-#             tile_value = puzzle.currentState[row][column]
-#             if (tile_value == 0):
-#                 continue
+        # tmp tile goal position should be on same column.
+        if (compare_digit_expected_column != expected_column):
+            continue
 
-#             (expected_row, expected_column) = solution.positions[tile_value]
-
-#             # We use XOR here cause we dont want case where tile is perfectly placed.
-#             if ((row == expected_row) ^ (column == expected_column)):
-#                 tiles_placed.append({"tile": tile, })
+        # if conflict
+        if ((index < row and row <= compare_digit_expected_row) or
+            (index > row and row >= compare_digit_expected_column)):
+            logging.debug(f"Vertical conflict between {digit} & {compare_digit}")
+            cost += 2
+    return cost
